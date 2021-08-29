3aee05528d7c10261bf98847c79dc742344792d9
Yuuki0210 <kni.hose@gmail.com>
2021-08-29 07:14:30 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+       redirect_to book_path(@book.id), notice: "You have created book successfully."
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @newbook = Book.new
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @user = current_user
+    if current_user != @book.user
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id), notice: "You have updated book successfully."
+    else
+       @book.update(book_params)
+       render :edit
+    end
+  end
+
+private
+
+def book_params
+  params.require(:book).permit(:title, :body)
+end
+
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'f2848f8417138b64e955b00441204c4a1f90ecf12b14dd2e31a7b690edb5d0a9b0032684abdd50b66e7dae60e15a7d52eb18e83a60cefa0226b428111cc32d69'
